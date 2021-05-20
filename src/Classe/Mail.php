1099f4f980fb80f5340bb54d6788491997b0230a
Faouzi <l.a.-noire@hotmail.com>
2021-05-20 16:17:02 +0200

    ajout fichiers manquants

---
+++ src/Classe/Mail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Classe;
+
+use Mailjet\Client;
+use Mailjet\Resources;
+
+class Mail
+{
+    private $api_key = '2905f0fd43c89104d2f9a4012fa5bdf1';
+    private $api_key_secret = 'a50271df6b6d2e2d759f391d3b5583c8';
+
+    public function send($to_email, $to_name, $subject, $content)
+    {
+        $mj = new Client($this->api_key, $this->api_key_secret, true, ['version' => 'v3.1']);
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => "faouzi.dakir@hotmail.com",
+                        'Name' => "La Boutique Française"
+                    ],
+                    'To' => [
+                        [
+                            'Email' => $to_email,
+                            'Name' => $to_name
+                        ]
+                    ],
+                    'TemplateID' => 2801504,
+                    'TemplateLanguage' => true,
+                    'Subject' => $subject,
+                    'Variables' => [
+                        'content' => $content,
+                    ]
+                ]
+            ]
+        ];
+        $response = $mj->post(Resources::$Email, ['body' => $body]);
+        $response->success();
+    }
+}
